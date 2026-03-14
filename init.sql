a1c01ff75ca756ee868efdf20a29949504601875
rafa-ruiz <rafa.ruiz@101obex.com>
2026-03-13 20:27:31 -0700

    Total 26 command available

--- init.sql
+++ init.sql
@@ -59,36 +59,84 @@ value = self.conector.variables.get(source, None)
 self.conector.results[source] = value
 $body$),
 
-('addParam', '[{"item":"param","type":"value"},{"item":"variable","type":"var"}]',
- 'param_name = task["properties"].get("param") or next(iter(task["properties"].values()), None)
-variable_name = task["properties"].get("variable") or next(reversed(task["properties"].values()), None)
+(
+    'addParam',
+    '[{"item":"param","type":"value"},{"item":"variable","type":"var"}]',
+    $body$
+import json
 
-value = None
-
-if hasattr(self.conector, "req"):
-    req = self.conector.req
+def procesar_dato(dato_binario):
+    if isinstance(dato_binario, (bytes, bytearray)):
+        texto = dato_binario.decode('utf-8')
+    else:
+        texto = str(dato_binario)
+    if texto.isdigit():
+        return int(texto)
     try:
-        # Tornado way: obtener query param
-        value = req.get_query_argument(param_name)
-    except tornado.web.MissingArgumentError:
+        return float(texto)
+    except ValueError:
+        return texto
+
+props = task["properties"]
+
+param    = props.get("param")     or next(iter(props.values()), None)
+try:
+    variable = props.get("Variable") or props.get("variable") or (list(props.values())[1] if len(props) > 1 else None)
+except:
+    variable = None
+
+param    = str(param).strip()
+variable = str(variable).strip() if variable else ''
+
+# Buscar en query arguments
+value_param = None
+for par in self.conector.req.request.query_arguments:
+    par1 = par.replace("'", '').replace('"', '')
+    if param == par1:
+        value_param = self.conector.req.request.query_arguments[par][0]
+        break
+
+# Si no se encontró en query, buscar en el body
+if value_param is None:
+    try:
+        body_data = json.loads(self.conector.req.body)
+        if param in body_data:
+            value_param = body_data[param]
+    except:
         try:
-            # Probar en body JSON
-            body_data = json.loads(req.request.body.decode())
-            value = body_data.get(param_name)
+            body_arguments = self.conector.req.body_arguments
+            if param in body_arguments:
+                value_param = body_arguments[param][0]
         except:
-            # Probar en body_arguments si existe
-            try:
-                value = req.body_arguments.get(param_name, [None])[0]
-            except:
-                pass
+            pass
 
-if variable_name:
-    self.conector.variables[variable_name] = value
-
-self.conector.logger.info(f"[AVAP PARSER] ADDING VARIABLE {variable_name} FROM PARAMETER {param_name} VALUE {value}")'),
-
-('if', '[{"item":"variable","type":"variable"},{"item":"variableValue","type":"variable"},{"item":"comparator","type":"value"}]',
+if value_param is not None and variable != '':
+    value_param = procesar_dato(value_param)
+    try:
+        value_param = value_param.decode("ascii", "ignore")
+    except:
+        pass
+    if value_param == '':
+        value_param = None
+    try:
+        value_param = json.loads(value_param)
+    except:
+        pass
+    self.conector.variables[variable] = value_param
+    globals()[variable] = value_param
+else:
+    if variable != '':
+        self.conector.variables[variable] = None
+        globals()[variable] = None
+    else:
+        self.conector.logger.warning("[AVAP] addParam - No variable name provided")
 $body$
+),
+
+(
+    'if',
+    '[{"item":"variable","type":"variable"},{"item":"variableValue","type":"variable"},{"item":"comparator","type":"value"}]',
+    $body$
 def try_num(v):
     s = str(v).strip().strip('"').strip("'")
     try:
@@ -96,27 +144,29 @@ def try_num(v):
     except:
         return s
 
-v1 = try_num(self.conector.variables.get(task["properties"]["variable"], task["properties"]["variable"]))
-v2 = try_num(task["properties"]["variableValue"])
+v1_raw = task["properties"]["variable"]
+v2_raw = task["properties"]["variableValue"]
+
+v1 = try_num(self.conector.variables.get(v1_raw, v1_raw))
+v2 = try_num(self.conector.variables.get(v2_raw, v2_raw))
 op = str(task["properties"]["comparator"]).strip().strip('"').strip("'")
 
 res = False
-if op in ["=", "=="]: res = (str(v1) == str(v2))
-elif op == ">": res = (v1 > v2)
-elif op == "<": res = (v1 < v2)
-elif op == ">=": res = (v1 >= v2)
-elif op == "<=": res = (v1 <= v2)
-elif op == "!=": res = (str(v1) != str(v2))
+if op in ["=", "=="]:   res = (str(v1) == str(v2))
+elif op == ">":          res = (float(v1) > float(v2))
+elif op == "<":          res = (float(v1) < float(v2))
+elif op == ">=":         res = (float(v1) >= float(v2))
+elif op == "<=":         res = (float(v1) <= float(v2))
+elif op == "!=":         res = (str(v1) != str(v2))
 
 branch = "true" if res else "false"
 if branch in task["branches"]:
     for step in task["branches"][branch]:
         self.process_step(step)
 
-# IMPORTANTE: Sincronizar de vuelta al finalizar la rama
-# Esto asegura que 'final' y otras variables lleguen al Executor principal
 self.conector.variables.update(self.conector.variables)
-$body$),
+$body$
+),
 
 ('end', '[]', 
 '# El comando end() marca el fin de un bloque condicional (if/else) o loop.
@@ -462,15 +512,390 @@ if rePattern in self.conector.variables:
 if newValue in self.conector.variables:
     newValue = self.conector.variables[newValue]
 
-patron = r"" + rePattern
+if not rePattern:
+    rePattern = ' '
 
-try:
-    result = re.sub(patron, newValue, SourceVariable)
-except re.error as e:
-    print(f"[AVAP] replace - Error en el patrón: {e}")
-    result = ""
+patron = r"" + rePattern.replace(' ', '\s')
+
+if not patron:
+    result = SourceVariable
+else:
+    try:
+        result = re.sub(patron, newValue, SourceVariable)
+    except re.error as e:
+        print(f"[AVAP] replace - Error en el patrón: {e}")
+        result = ""
 
 self.conector.variables[TargetVariable] = result.replace('¨', '"')
+$body$
+),
+(
+    'ormDirect',
+    '[{"item":"prompt","type":"var"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+from sqlalchemy import text
+
+props = task["properties"]
+
+prompt         = props.get("prompt")         or next(iter(props.values()), None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[1] if len(props) > 1 else None)
+
+prompt         = str(prompt).strip()
+TargetVariable = str(TargetVariable).strip()
+
+if prompt in self.conector.variables:
+    prompt = self.conector.variables[prompt]
+elif prompt in locals():
+    prompt = locals()[prompt]
+
+prompt = prompt.replace("`", "'")
+
+try:
+    result_query = self.conector.conexion.execute(text(prompt))
+
+    if result_query.returns_rows:
+        result_rows = result_query.mappings().all()
+        result_json = [dict(row) for row in result_rows]
+        self.conector.variables[TargetVariable] = result_json
+    else:
+        if not getattr(self.conector, 'sandbox', False):
+            self.conector.conexion.commit()
+        self.conector.variables[TargetVariable] = f"Success: {result_query.rowcount}"
+
+except Exception as e:
+    print(f"[AVAP] ormDirect - Error: {e}")
+    self.conector.variables[TargetVariable] = {"error": str(e)}
+$body$
+),(
+    'getTimeStamp',
+    '[{"item":"DateString","type":"var"},{"item":"Format","type":"var"},{"item":"TimeDelta","type":"var"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import datetime
+
+props = task["properties"]
+
+DateString     = props.get("DateString")     or next(iter(props.values()), None)
+Format         = props.get("Format")         or (list(props.values())[1] if len(props) > 1 else None)
+TimeDelta      = props.get("TimeDelta")      or (list(props.values())[2] if len(props) > 2 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[3] if len(props) > 3 else None)
+
+DateString     = str(DateString).strip()
+Format         = str(Format).strip()
+TimeDelta      = str(TimeDelta).strip()
+TargetVariable = str(TargetVariable).strip()
+
+if DateString in self.conector.variables:
+    DateString = str(self.conector.variables[DateString]).strip()
+elif DateString in locals():
+    DateString = str(locals()[DateString]).strip()
+
+if type(DateString) is datetime.datetime:
+    DateString = DateString.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+currentDateAndTime = datetime.datetime.strptime(DateString, Format)
+
+if TimeDelta != '0':
+    Delta = datetime.timedelta(seconds=int(TimeDelta))
+    currentDateAndTime = currentDateAndTime + Delta
+
+formateDaterime = currentDateAndTime.timestamp()
+
+self.conector.variables[TargetVariable] = formateDaterime
+$body$
+),
+(
+    'getRegex',
+    '[{"item":"SourceVariable","type":"variable"},{"item":"rePattern","type":"variable"},{"item":"TargetVariable","type":"variable"}]',
+    $body$
+import re
+
+props = task["properties"]
+
+SourceVariable = props.get("SourceVariable") or next(iter(props.values()), None)
+rePattern      = props.get("rePattern")      or (list(props.values())[1] if len(props) > 1 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[2] if len(props) > 2 else None)
+
+SourceVariable = str(SourceVariable).strip()
+rePattern      = str(rePattern).strip()
+TargetVariable = str(TargetVariable).strip()
+
+if SourceVariable in self.conector.variables:
+    SourceVariable = self.conector.variables[SourceVariable]
+if rePattern in self.conector.variables:
+    rePattern = self.conector.variables[rePattern]
+
+__patron = r"" + rePattern
+__m = re.findall(__patron, SourceVariable)
+__result = "".join(__m) if __m else None
+
+self.conector.variables[TargetVariable] = __result
+$body$
+), (
+    'getDateTime',
+    '[{"item":"Format","type":"var"},{"item":"TimeDelta","type":"var"},{"item":"TimeZone","type":"var"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import datetime
+import pytz
+
+props = task["properties"]
+
+Format         = props.get("Format")         or next(iter(props.values()), None)
+TimeDelta      = props.get("TimeDelta")      or (list(props.values())[1] if len(props) > 1 else None)
+TimeZone       = props.get("TimeZone")       or (list(props.values())[2] if len(props) > 2 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[3] if len(props) > 3 else None)
+
+Format         = str(Format).strip()
+TimeDelta      = str(TimeDelta).strip()
+TimeZone       = str(TimeZone).strip()
+TargetVariable = str(TargetVariable).strip()
+
+datetimeZoneString = None
+
+if TimeZone != "":
+    if TimeZone not in ("current", "Current"):
+        try:
+            datetimeZoneString = pytz.timezone(TimeZone)
+        except:
+            datetimeZoneString = None
+
+currentDateAndTime = datetime.datetime.now(datetimeZoneString)
+
+if TimeDelta != '0':
+    Delta = datetime.timedelta(seconds=int(TimeDelta))
+    currentDateAndTime = currentDateAndTime + Delta
+
+if Format != "":
+    formateDaterime = currentDateAndTime.strftime(Format)
+else:
+    formateDaterime = currentDateAndTime.timestamp()
+
+self.conector.variables[TargetVariable] = formateDaterime
+$body$
+), (
+    'getQueryParamList',
+    '[{"item":"TargetVariable","type":"var"}]',
+    $body$
+props = task["properties"]
+
+TargetVariable = props.get("TargetVariable") or next(iter(props.values()), None)
+TargetVariable = str(TargetVariable).strip()
+
+param_list = []
+for key in self.conector.req.query_arguments.keys():
+    valor = self.conector.req.query_arguments[key][0]
+    param_list.append({key: valor})
+
+self.conector.variables[TargetVariable] = param_list
+$body$
+), (
+    'getListLen',
+    '[{"item":"SourceVariable","type":"var"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import ast
+import os
+
+try:
+    __DEBUG = os.getenv("DEBUG")
+    if __DEBUG == "True": __DEBUG = True
+except:
+    __DEBUG = True
+
+props = task["properties"]
+
+SourceVariable = props.get("SourceVariable") or next(iter(props.values()), None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[1] if len(props) > 1 else None)
+
+SourceVariable = str(SourceVariable).strip()
+TargetVariable = str(TargetVariable).strip()
+
+__source_value = []
+try:
+    __source_value = self.conector.variables[SourceVariable]
+except:
+    try:
+        __source_value = self.conector.local_vars[SourceVariable]
+    except:
+        try:
+            __source_value = locals()[SourceVariable]
+        except:
+            try:
+                __source_value = globals()[SourceVariable]
+            except:
+                __source_value = []
+
+try:
+    es = ast.literal_eval(__source_value)
+    longitud = len(es)
+except:
+    longitud = len(__source_value)
+
+if __DEBUG == True: print("[AVAP] getListLen LENGTH %s" % longitud)
+
+self.conector.variables[TargetVariable] = longitud
+
+if __DEBUG == True: print("[AVAP] getListLen TARGET VAR %s" % TargetVariable)
+if __DEBUG == True: print("[AVAP] getListLen TARGET VALUE %s" % self.conector.variables[TargetVariable])
+$body$
+),
+(
+    'itemFromList',
+    '[{"item":"SourceVariable","type":"var"},{"item":"index","type":"value"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import ast
+
+props = task["properties"]
+
+SourceVariable = props.get("SourceVariable") or next(iter(props.values()), None)
+raw_index      = props.get("index")          or (list(props.values())[1] if len(props) > 1 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[2] if len(props) > 2 else None)
+
+SourceVariable = str(SourceVariable).strip()
+TargetVariable = str(TargetVariable).strip()
+raw_index      = str(raw_index).strip()
+
+# Resolver index
+if raw_index.isdigit():
+    index = int(raw_index)
+elif raw_index in self.conector.variables:
+    index = self.conector.variables[raw_index]
+else:
+    index = int(raw_index)
+
+# Resolver valor origen
+try:
+    valorOrigen = ast.literal_eval(self.conector.variables[SourceVariable])
+except:
+    valorOrigen = self.conector.variables[SourceVariable]
+
+self.conector.logger.info(
+    "[AVAP] itemFromList - Getting index %s from %s" % (index, SourceVariable)
+)
+
+self.conector.variables[TargetVariable] = valorOrigen[index]
+$body$
+), (
+    'stampToDatetime',
+    '[{"item":"timestamp","type":"var"},{"item":"Format","type":"var"},{"item":"TimeDelta","type":"var"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import datetime
+
+props = task["properties"]
+
+_time__stamp   = props.get("timestamp")      or next(iter(props.values()), None)
+Format         = props.get("Format")         or (list(props.values())[1] if len(props) > 1 else None)
+TimeDelta      = props.get("TimeDelta")      or (list(props.values())[2] if len(props) > 2 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[3] if len(props) > 3 else None)
+
+_time__stamp   = str(_time__stamp).strip()
+Format         = str(Format).strip()
+TimeDelta      = str(TimeDelta).strip()
+TargetVariable = str(TargetVariable).strip()
+
+if _time__stamp in self.conector.variables:
+    _time__stamp = str(self.conector.variables[_time__stamp]).strip()
+elif _time__stamp in locals():
+    _time__stamp = str(locals()[_time__stamp]).strip()
+
+currentDateAndTime = datetime.datetime.fromtimestamp(float(_time__stamp))
+
+if TimeDelta != '0':
+    Delta = datetime.timedelta(seconds=int(TimeDelta))
+    currentDateAndTime = currentDateAndTime + Delta
+
+if Format != "":
+    formateDaterime = currentDateAndTime.strftime(Format)
+else:
+    formateDaterime = currentDateAndTime.timestamp()
+
+self.conector.variables[TargetVariable] = formateDaterime
+$body$
+) , (
+    'AddvariableToJSON',
+    '[{"item":"Key","type":"variable"},{"item":"Value","type":"variable"},{"item":"TargetVariable","type":"variable"}]',
+    $body$
+import re
+
+props = task["properties"]
+
+__Key            = props.get("Key")            or next(iter(props.values()), None)
+__Value          = props.get("Value")          or (list(props.values())[1] if len(props) > 1 else None)
+__TargetVariable = props.get("TargetVariable") or (list(props.values())[2] if len(props) > 2 else None)
+
+__Key            = str(__Key).strip()
+__Value          = str(__Value).strip()
+__TargetVariable = str(__TargetVariable).strip()
+
+# Inicializar TargetVariable como dict si no existe o no es dict
+if __TargetVariable not in self.conector.variables:
+    self.conector.variables[__TargetVariable] = {}
+if not isinstance(self.conector.variables[__TargetVariable], dict):
+    self.conector.variables[__TargetVariable] = {}
+
+# Interpolación de template en __Key
+try:
+    if __Key.strip()[0] == '`' and __Key.strip()[-1] == '`':
+        __Key = __Key.replace('`', '')
+        __variables_cadena = re.findall('\${([^}]*)}', __Key)
+        for __varia in __variables_cadena:
+            __kert = "${%s}" % __varia
+            if __varia in self.conector.variables:
+                __Key = __Key.replace(__kert, self.conector.variables[__varia])
+            elif __varia in self.conector.local_vars:
+                __Key = __Key.replace(__kert, self.conector.local_vars[__varia])
+            else:
+                __Key = __Key.replace(__kert, '')
+except Exception as e:
+    print(f"[AVAP] AddvariableToJSON - Key interpolation error: {e}")
+
+# Interpolación de template en __Value
+try:
+    if __Value.strip()[0] == '`' and __Value.strip()[-1] == '`':
+        __Value = __Value.replace('`', '')
+        __variables_cadena = re.findall('\${([^}]*)}', __Value)
+        for __varia in __variables_cadena:
+            __kert = "${%s}" % __varia
+            if str(__varia) in self.conector.variables:
+                __Value = __Value.replace(__kert, str(self.conector.variables[__varia]))
+            elif __varia in self.conector.local_vars:
+                __Value = __Value.replace(__kert, str(self.conector.local_vars[__varia]))
+            else:
+                __Value = __Value.replace(__kert, '')
+except Exception as e:
+    print(f"[AVAP] AddvariableToJSON - Value interpolation error: {e}")
+
+# Resolver referencias a variables
+if __Value in self.conector.variables:
+    __Value = self.conector.variables[__Value]
+if __Key in self.conector.variables:
+    __Key = self.conector.variables[__Key]
+
+self.conector.variables[__TargetVariable][__Key] = __Value
+$body$
+) , (
+    'variableFromJSON',
+    '[{"item":"SourceVariable","type":"var"},{"item":"key","type":"value"},{"item":"TargetVariable","type":"var"}]',
+    $body$
+import json
+
+props = task["properties"]
+
+SourceVariable = props.get("SourceVariable") or next(iter(props.values()), None)
+key            = props.get("key")            or (list(props.values())[1] if len(props) > 1 else None)
+TargetVariable = props.get("TargetVariable") or (list(props.values())[2] if len(props) > 2 else None)
+
+SourceVariable = str(SourceVariable).strip()
+key            = str(key).strip()
+TargetVariable = str(TargetVariable).strip()
+
+try:
+    valorOrigen = json.loads(self.conector.variables[SourceVariable])
+except:
+    valorOrigen = self.conector.variables[SourceVariable]
+
+try:
+    self.conector.variables[TargetVariable] = valorOrigen[key]
+except:
+    self.conector.variables[TargetVariable] = None
 $body$
 )
 ;
